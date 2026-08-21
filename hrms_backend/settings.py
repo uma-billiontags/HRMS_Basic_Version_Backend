c6c08f6808b3b1afa830ba135d07be2be9704f38
uma-billiontags <uma@billiontags.co>
2026-08-21 16:36:43 +0530

    Initial Setup

--- hrms_backend/settings.py
+++ hrms_backend/settings.py
@@ -113,12 +113,12 @@ WSGI_APPLICATION = 'hrms_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -138,31 +138,31 @@ WSGI_APPLICATION = 'hrms_backend.wsgi.application'
 #     }
 # }
 
-import os
-from pathlib import Path
+# import os
+# from pathlib import Path
 
-ca_cert_content = os.environ.get('DB_CA_CERT')
-ca_path = BASE_DIR / 'ca.pem'
+# ca_cert_content = os.environ.get('DB_CA_CERT')
+# ca_path = BASE_DIR / 'ca.pem'
 
-if ca_cert_content and not ca_path.exists():
-    ca_path.write_text(ca_cert_content)
+# if ca_cert_content and not ca_path.exists():
+#     ca_path.write_text(ca_cert_content)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-        'CONN_MAX_AGE': 60,
-        'CONN_HEALTH_CHECKS': True,
-        'OPTIONS': {
-            'ssl': {'ca': str(ca_path)} if ca_path.exists() else {},
-            'connect_timeout': 10,
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#         'CONN_MAX_AGE': 60,
+#         'CONN_HEALTH_CHECKS': True,
+#         'OPTIONS': {
+#             'ssl': {'ca': str(ca_path)} if ca_path.exists() else {},
+#             'connect_timeout': 10,
+#         },
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
